38758212d86592355813db49584a746249a0eeed
linhnv <nvlk12haui@gmail.com>
2023-06-28 22:59:46 +0700

    init

---
+++ auth-server/src/test/java/com/linhnv/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhnv.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
